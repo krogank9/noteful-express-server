44102d6c3f5242c051c22aab0d669f23df4ffa29
krogank9 <wifi.mouse.app@gmail.com>
2019-10-14 16:27:54 -0400

    update for heroku

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ module.exports = {
 	PORT: process.env.PORT || 8000,
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	DATABASE_URL: process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : process.env.TEST_DATABASE_URL,
+	"ssl": !!process.env.SSL,
 }
