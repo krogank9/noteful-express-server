7c2945290fb4cc90acea535a86f996306cd48c8a
krogank9 <wifi.mouse.app@gmail.com>
2019-10-14 16:38:47 -0400

    update for heroku

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	PORT: process.env.PORT || 8000,
 	NODE_ENV: process.env.NODE_ENV || 'development',
-	DATABASE_URL: process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : process.env.TEST_DATABASE_URL,
-	"ssl": !!process.env.SSL,
+	DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful',
+	TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/noteful-test',
 }
