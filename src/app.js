e5985795d956f8ff0ee3b8aa1cebb2d96984bd0e
krogank9 <wifi.mouse.app@gmail.com>
2019-10-14 17:32:41 -0400

    fix cors

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const foldersRouter = require('./folders/folders-router')
 const app = express()
 
 var corsOptions = {
-	origin: '*',
+	origin: 'http://localhost:3000',
 	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
